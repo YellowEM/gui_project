6e7d41111b43ee23e1e5f34bce0250036b33501e
Michael <drgnom95@gmail.com>
2023-06-05 20:09:33 +0300

    add calc and less7

---
+++ lesson7.py
@@ -0,0 +1,52 @@
+# Знакомство с виджетами. Checkbutton
+
+import tkinter as tk
+
+
+def select_all():
+    for check in (over_18, commercial, follow):
+        check.select()
+
+
+def deselect_all():
+    for check in (over_18, commercial, follow):
+        check.deselect()
+
+
+def switch_all():
+    for check in (over_18, commercial, follow):
+        check.toggle()
+
+
+def show():
+    print('флажок 18', over_18_value.get())
+    print('реклама', commercial_value.get())
+
+
+win = tk.Tk()
+
+over_18_value = tk.StringVar()
+over_18_value.set('No')
+commercial_value = tk.IntVar()
+win.title('Graph')
+win.geometry(f'300x400+100+200')
+
+over_18 = tk.Checkbutton(win, text='Вам исполнилось 18?',
+                         variable=over_18_value,
+                         offvalue='No',
+                         onvalue='Yes')
+commercial = tk.Checkbutton(win, text='Хотиле получать рекламу?',
+                            variable=commercial_value,
+                            offvalue=0,
+                            onvalue=1)
+follow = tk.Checkbutton(win, text='Хотите подписаться на канал?', indicatoron=0)
+
+over_18.pack()
+commercial.pack()
+follow.pack()
+tk.Button(win, text='select_all', command=select_all).pack()
+tk.Button(win, text='deselect_all', command=deselect_all).pack()
+tk.Button(win, text='switch_all', command=switch_all).pack()
+tk.Button(win, text='Show', command=show).pack()
+
+win.mainloop()
