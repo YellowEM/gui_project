48dd9bd957939d3e481df789858e3d7390cc85a4
Michael <drgnom95@gmail.com>
2023-06-06 19:58:56 +0300

    less8 Radiobutton

---
+++ lesson8.py
@@ -0,0 +1,27 @@
+import  tkinter as tk
+
+
+levels = {
+    1:"Easy",
+    2:"Middle",
+    3:"Hard",
+}
+def select_level():
+    level = level_var.get()
+    level_text.set(f'Вы выбрали {level} уровень сложности')
+    print(levels[level])
+win = tk.Tk()
+win.geometry(f'300x400+100+200')
+win.title('Graph')
+
+level_var = tk.IntVar()
+nation_var = tk.IntVar()
+level_text = tk.StringVar()
+
+tk.Label(win,text='Выберте уровень сложности').pack()
+for level in sorted(levels):
+    tk.Radiobutton(win, text=levels[level], variable=level_var, value=level, command=select_level).pack()
+
+tk.Label(win, textvariable=level_text).pack()
+
+win.mainloop()
